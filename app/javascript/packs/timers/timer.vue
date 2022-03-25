0fc441a36da6dd1c3545b8fe938d52524039651c
Iori OSADA <iori.osada@gmail.com>
2022-03-25 17:59:08 +0900

    fix: style

--- app/javascript/packs/timers/timer.vue
+++ app/javascript/packs/timers/timer.vue
@@ -1,31 +1,37 @@
 <template>
-  <div>
-  Token: {{ token }}
-  </div>
-  <div>
-  Link: {{ link }}
-  </div>
-  <div>
-  {{ timerString }}
-  </div>
-  <button v-on:click="resetTimer">
-    reset!
-  </button>
-  <div v-if="debug">
-    <div>
-    {{ debug }}
-    </div>
-    <div>
-      {{ timerJson }}
-    </div>
-    <div>
-      {{ startTime }}
-    </div>
-    <div>
-      {{ currentTime }}
-    </div>
-    <div>
-      {{ loading }}
+  <div class="container">
+    <div class="content">
+      <div class="information">
+        <div class="information__item">
+        Token: {{ token }}
+        </div>
+        <div class="information__item">
+        Link: {{ link }}
+        </div>
+        <div v-if="debug">
+          <div class="information__item">
+            Debug: {{ debug }}
+          </div>
+          <div class="information__item">
+            {{ timerJson }}
+          </div>
+          <div class="information__item">
+            {{ startTime }}
+          </div>
+          <div class="information__item">
+            {{ currentTime }}
+          </div>
+          <div class="information__item">
+            {{ loading }}
+          </div>
+        </div>
+      </div>
+      <div class='timer'>
+      {{ timerString }}
+      </div>
+      <button v-on:click="resetTimer" class="reset-button">
+        reset!
+      </button>
     </div>
   </div>
 </template>
@@ -121,4 +127,69 @@ export default {
 </script>
 
 <style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  color: #666;
+  box-sizing: content-box;
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  background: #333;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.content {
+  position: fixed;
+  top: calc(50% - 95vh/2);
+  left: calc(50% - 80vw/2);
+  width: 80vw;
+  height: auto;
+  overflow-y: scroll;
+
+  padding: 50px;
+
+  border-radius: 20px;
+  background: #eee;
+
+  font-family: "Nitalagoruika-05", "Aktiv Grotesk","Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+  text-align: center;
+}
+
+.information {
+  border: 1px #ccc solid;
+  border-radius: 20px;
+}
+
+.timer {
+  margin: 20px auto;
+  font-size: 10rem;
+}
+
+.reset-button {
+  font-size: 3rem;
+  padding: .5rem 1rem;
+
+  text-decoration: none;
+  border-radius: 10px;
+  display: inline;
+  cursor: pointer;
+
+  border: 1px solid #666;
+  color: #666;
+  background: #fff;
+
+  transition: 0.3s;
+}
+
+.reset-button:hover {
+  color: #fff;
+  background: #666;
+}
+
 </style>
